6ae2f980334d7fdbdb076ec1d3dfc3dd8cfde30b
gsmac14 <gsmac1224@gmail.com>
2021-08-23 21:54:33 -0500

    added backend

---
+++ client/src/utils/API.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-anonymous-default-export */
+import axios from "axios";
+
+export default{
+
+    //Logs in user post route
+    getUser: function (userData) {
+    return axios.post("/api/auth/login", userData);
+},
+    //Logs current user out when promted
+    logOut: function(userData){
+    return axios.post("/api/auth/logout")
+},
+    //creates a new account for a user 
+    createUser: function(userData){
+        return axios.post("/api/auth/register", userData);
+},
+
+//     eventPost: function(userData){
+//         return axios.post("/api/event/createEvent", userData);
+//     },
+
+//     getPost: function(id) {
+//         return axios.get("/api/event/" + id);
+    
+//     },
+
+//     // Gets all posts
+//     getPosts: function(userData) {
+//     return axios.get("/api/event/createEvent", userData);
+//     },
+
+//     deletePost: function(id){
+//         return axios.delete("/api/event/" + id);
+//     }
+// }
---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const AuthRoutes = require('./users');
+
+
+// Authentication routes
+router.use("/auth", AuthRoutes);
+
+
+
+module.exports = router;
