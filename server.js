cd47c9abe99247340c1bb02d5b59c08d44db1190
gsmac14 <gsmac1224@gmail.com>
2021-08-26 22:26:34 -0500

    added navbar with links

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
 const path= require("path");
+const crypto = requre('crypto');//to generate file name
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -22,6 +26,7 @@ if (process.env.NODE_ENV === "production") {
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://gmac:CodeMansa21!@cluster0.xnd6h.mongodb.net/thera2go?retryWrites=true&w=majority");
+
 
 // Start the API server
 app.listen(PORT, function() {
